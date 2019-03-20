8c57170cff1ffdd0a9b14ed990262e2fd2df0b6b
westonsfiala <westonsfiala@gmail.com>
2019-03-19 22:36:23 -0700

    Enabled color output for the display. More work on that later.

--- src/user_interface.cc
+++ src/user_interface.cc
@@ -8,15 +8,24 @@ UserInterface::UserInterface(BattleRunner& runner) : Process("user input"), mRun
     timeout(1);  // Timeout for waiting for user input
     noecho();    // Do not echo user input to the screen
     curs_set(0); // Do not show the cursor
+    start_color(); // Start color up.
+    
+    init_pair(1, COLOR_RED, COLOR_BLACK);
+    init_pair(2, COLOR_YELLOW, COLOR_BLACK);
+    init_pair(3, COLOR_WHITE, COLOR_BLACK);
 };
 
 void UserInterface::update() 
 {
     clear();
 
+    attron(COLOR_PAIR(1));
     mvprintw(1,1,mRunner.get_state_title().c_str());
+
+    attron(COLOR_PAIR(2));
     mvprintw(2,1,mRunner.get_action_string().c_str());
 
+    attron(COLOR_PAIR(3));
     auto line = 4;
     for(auto displayLine : mRunner.get_display())
     {
